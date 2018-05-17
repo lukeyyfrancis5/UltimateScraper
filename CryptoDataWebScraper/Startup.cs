69f96b11f9809915564d296bf6a75a54c670117a
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-05-17 19:10:00 -0400

    Add project files.

---
+++ CryptoDataWebScraper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CryptoDataWebScraper.Startup))]
+namespace CryptoDataWebScraper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
